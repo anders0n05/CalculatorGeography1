dc4ca57b30cf76a34745d6b0e60a14af6477a965
ITJ-ANDERSON <willianandi@outlook.com>
2019-09-25 19:57:59 -0300

    Ajustando Circulo

--- app/src/main/java/com/example/calculatorgeography/InputRaioCirculo.java
+++ app/src/main/java/com/example/calculatorgeography/InputRaioCirculo.java
@@ -2,13 +2,36 @@ package com.example.calculatorgeography;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InputRaioCirculo extends AppCompatActivity {
-
+    private EditText Raio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_raio_circulo);
+
+        Button AvancarRaio = findViewById(R.id.AvancarRaio);
+
+        Raio = findViewById(R.id.RaioInput);
+
+
+
+    }
+
+
+    public void onclickBotaoEnviarCirculo(View view) {
+        String circle = Raio.getText().toString();
+        Intent EnviaResposta = new Intent( getApplicationContext(),PrintAreaCirculo.class);
+        Bundle parametros = new Bundle();
+
+        parametros.putString("chave_circle", circle);
+
+        EnviaResposta.putExtras(parametros);
+        startActivity(EnviaResposta);
     }
 }
--- app/src/main/java/com/example/calculatorgeography/PrintAreaCirculo.java
+++ app/src/main/java/com/example/calculatorgeography/PrintAreaCirculo.java
@@ -2,13 +2,41 @@ package com.example.calculatorgeography;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PrintAreaCirculo extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_print_area_circulo);
+        EditText texto = (EditText)findViewById(R.id.AreaRespostaCircle);
+        Button Voltar = findViewById(R.id.VoltarCircle);
+        Voltar.setOnClickListener(new View.OnClickListener() { // voltando tela principal
+            @Override
+            public void onClick(View v) {
+                Intent TelaPrincipal= new Intent(PrintAreaCirculo.this, MainActivity.class);
+                startActivity(TelaPrincipal);
+                finish();
+            }
+        });
+        Intent Recebe = getIntent();
+        Bundle parametros  = Recebe.getExtras();
+        if (parametros!=null){
+            String raio= parametros.getString("chave_circle");
+            Integer.parseInt(raio);
+           int raio_int = Integer.parseInt(raio);
+
+
+            int area = (int) ((raio_int*raio_int) * Math.PI);
+            // texto.setText(base);
+            texto.setText(String.valueOf(area)) ;
+           // texto.setText(raio);
+
+        }
     }
 }
